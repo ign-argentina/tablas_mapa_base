591b5341b7f8c61c565443b4228bdd7b0b44223f
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2021-11-08 17:42:10 -0300

    conversión de ref mejorada

--- red_vial.sql
+++ red_vial.sql
@@ -7,36 +7,19 @@ SELECT
   other_tags -> 'alt_name' AS alt_name,
   other_tags -> 'short_name' AS short_name,
   other_tags -> 'ref' AS ref,
-  (
-    CASE
-      WHEN other_tags :: hstore -> 'ref' IS NULL THEN NULL
-      WHEN other_tags :: hstore -> 'ref' LIKE '%;%' THEN NULL
-      WHEN other_tags :: hstore -> 'ref' LIKE '%-%' THEN NULLIF(
-        regexp_replace(
-          LEFT(
-            other_tags :: hstore -> 'ref',
-            STRPOS(other_tags :: hstore -> 'ref', '-') - 1
-          ),
-          '\D',
-          '',
-          'g'
-        ),
-        ''
-      )
-      ELSE NULLIF(
-        regexp_replace(other_tags :: hstore -> 'ref', '\D', '', 'g'),
-        ''
-      )
-    END
-  ) :: numeric AS num_ref,
+  CASE 
+		WHEN other_tags :: hstore -> 'ref' NOT LIKE '%;%' 
+		THEN replace(TRIM(regexp_replace((other_tags :: hstore -> 'ref'), '(RP)|(RN)', '', 'g')), ' ', '-')
+  END AS num_ref,
+  other_tags -> 'oneway' AS oneway,
   other_tags -> 'access' AS access,
   other_tags -> 'bridge' AS bridge,
   other_tags -> 'tunnel' AS tunnel,
-  other_tags -> 'layer' AS layer,
-  other_tags -> 'maxspeed' AS maxspeed,
-  -- other_tags -> 'lanes' AS lanes,
-  -- other_tags -> 'surface' AS surface,
-  -- other_tags -> 'smoothness' AS smoothness,
+  CAST(other_tags -> 'layer' AS INT) AS layer,
+  CAST(other_tags -> 'maxspeed' AS INT) AS maxspeed,
+  CAST(other_tags -> 'lanes' AS INT) AS lanes,
+  other_tags -> 'surface' AS surface,
+  other_tags -> 'smoothness' AS smoothness,
   ST_SetSRID(ST_Transform(wkb_geometry, 3857),3857) AS geom
 FROM
   osm.lines
