1d53f54c7b920da75fcf732c70f86e2fc16c760f
zalitoar <3695043+Zalitoar@users.noreply.github.com>
2026-08-13 10:29:53 -0300

    se agregan objetos y se borran nombres en otros idiomas

--- osm_plazas.sql
+++ osm_plazas.sql
@@ -1,15 +1,28 @@
+UPDATE osm.multipolygons
+SET name = NULL
+WHERE
+	name IS NOT NULL
+AND
+    ST_Intersects(
+      ST_MakeEnvelope(-55.569620, -50.408518, -63.156454,-53.416080, 4326), 
+      wkb_geometry
+    );
+
 DROP TABLE IF EXISTS argenmap.osm_plazas CASCADE;
 CREATE TABLE argenmap.osm_plazas AS
 SELECT
   name,
   leisure,
+  landuse,
   other_tags -> 'alt_name' AS alt_name,
   other_tags -> 'short_name' AS short_name,
   ST_SetSRID(ST_Transform(wkb_geometry, 3857),3857) AS geom
 FROM
   osm.multipolygons
 WHERE
-  leisure = 'park';
+  leisure IN ('park', 'pitch', 'garden', 'sports_centre', 'track', 'nature_reserve', 'stadium', 'golf_course')
+OR 
+  landuse IN ('grass', 'cemetery', 'recreation_ground', 'forest', 'meadow', 'orchard', 'farmyard');
 GRANT
 SELECT
   ON TABLE argenmap.osm_plazas TO readonly;
